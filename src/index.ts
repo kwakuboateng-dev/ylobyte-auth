599d054022e6e8be20bdbfea4052644b31e9ca82
Kwaku Boateng <kwakuboateng.dev@outlook.com>
2023-05-24 18:52:24 +0000

    server init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello Ylobyte 🥚!");
+// console.log("Hello Ylobyte 🥚!");
+
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import http from "http";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(compression());
+app.use(cookieParser());
+
+const server = http.createServer(app);
+
+server.listen(8100, () => {
+  console.log("Server running on http://localhost:8100");
+});
